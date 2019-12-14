d08242fc772ab39ce9c7a656ddfb3bdc123c04b9
Brandon Weaver <a7xncob@gmail.com>
2019-12-14 19:56:43 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,15 +2,15 @@ class Genre < ActiveRecord::Base
   
   has_many :songs
   has_many :artists, through: :songs
-
+  
   def song_count
     return self.songs.size
   end
-
+  
   def artist_count
     return self.artists.size
   end
-
+  
   def all_artist_names
     names = []
     self.artists.each do |artist|
