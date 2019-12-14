3a5e547fa0741919010a845ab8af96be03e863d1
Brandon Weaver <a7xncob@gmail.com>
2019-12-14 19:56:35 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ActiveRecord::Base
+  
   has_many :songs
   has_many :artists, through: :songs
 
@@ -18,4 +19,5 @@ class Genre < ActiveRecord::Base
     
     return names
   end
+  
 end
